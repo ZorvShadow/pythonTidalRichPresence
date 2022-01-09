4379cb60a498937a577dfa4566ddecfedef33d00
ZorvShadow <progamerpooprabah@gmail.com>
2022-01-08 18:29:52 -0600

    Updated tidal.py

--- tidal.py
+++ tidal.py
@@ -1,12 +1,11 @@
-
-#Currently not working 07/01/2022
+#Currently not used
 
 import tidalapi
 
 session = tidalapi.Session()
 # Will run until you visit the printed url and link your account
 session.login_oauth_simple()
-album = session.album(16909093)
+album = session.album(109485854)
 tracks = album.tracks()
 for track in tracks:
     print(track.name)
