1f00c2735fc81b83463c1315ba6d32b9bc38e0c6
ZorvShadow <progamerpooprabah@gmail.com>
2022-01-08 19:07:20 -0600

    Updated tests on tidal.py

--- tidal.py
+++ tidal.py
@@ -1,11 +1,10 @@
 #Currently not used
+#Just for testing
 
 import tidalapi
 
 session = tidalapi.Session()
 # Will run until you visit the printed url and link your account
 session.login_oauth_simple()
-album = session.album(109485854)
-tracks = album.tracks()
-for track in tracks:
-    print(track.name)
+
+print(session.search("IGOR",models=[tidalapi.Album],limit=50,offset=0))
