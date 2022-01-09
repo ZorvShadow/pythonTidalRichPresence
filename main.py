bd4da30ac37e92a7ae285204ed66a86b003172be
ZorvShadow <progamerpooprabah@gmail.com>
2022-01-08 18:53:42 -0600

    Re-added rpc

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import time
 import win32gui
 from pypresence import Presence
 
-#TidalPid = hwndManager.findTidalPIDs()[0]
+TidalPid = hwndManager.findTidalPIDs()[0]
 
-#print("TidalPid = ",TidalPid)
+print("TidalPid = ",TidalPid)
 
 hwnd = hwndManager.get_hwndTidal()
 
-#rpc = Presence("927718553837256784")
-#rpc.connect()
+rpc = Presence("927718553837256784")
+rpc.connect()
 
 
 while True:
@@ -25,11 +25,11 @@ while True:
         trackName=textList[0]
         artistName=textList[1][1:]
 
-       # rpc.update(pid=TidalPid, state="by: " + artistName, details="Track: " + trackName, large_image="tidal")
+        rpc.update(pid=TidalPid, state="by: " + artistName, details="Track: " + trackName, large_image="tidal")
 
         print("Track: ", trackName)
         print("by:    ", artistName)
     else:
-      #  rpc.clear(pid=TidalPid)
+        rpc.clear(pid=TidalPid)
         print("No music is currently playing")
  
