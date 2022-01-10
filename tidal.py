67e18584bbd62f8f9efedb7eeb18ecf1f6288e66
ZorvShadow <progamerpooprabah@gmail.com>
2022-01-10 15:50:05 -0600

    Updated working tidal.py

--- tidal.py
+++ tidal.py
@@ -2,9 +2,23 @@
 #Just for testing
 
 import tidalapi
+import webbrowser
 
 session = tidalapi.Session()
 # Will run until you visit the printed url and link your account
-session.login_oauth_simple()
+session.login_oauth_simple(function=webbrowser.open)
 
-print(session.search("IGOR",models=[tidalapi.Album],limit=50,offset=0))
+def searchTrack(searchquerry):
+
+    artists = ""
+
+    trackDict = session.search(searchquerry,models=[tidalapi.media.Track],limit=50,offset=0)
+
+    track=trackDict.get("top_hit")
+
+    print(track.name)
+    print(len(track.artists))
+    #for x in track.artists:
+       # print(x.name)
+searchTrack("edamame")  
+
